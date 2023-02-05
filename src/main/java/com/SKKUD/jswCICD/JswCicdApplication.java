c1b938e3eb46ceebef77ced9013ff92aff1dd05c
정선우 <sunwoo@jeongseon-uui-MacBookAir.local>
2023-02-05 15:09:57 +0900

    CICD TestCode

---
+++ src/main/java/com/SKKUD/jswCICD/JswCicdApplication.java
@@ -0,0 +1,13 @@
+package com.SKKUD.jswCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JswCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JswCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SKKUD/jswCICD/JswCicdApplicationTests.java
@@ -0,0 +1,14 @@
+package com.SKKUD.jswCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JswCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Success");
+	}
+
+}
